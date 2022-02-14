464094981898e28f455ef93553f0de672b9885ac
Iker Mariñelarena <iker@540deg.com>
2022-02-14 22:22:36 +0100

    [] - Initial state

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -11,13 +11,6 @@ final class ExampleTest extends TestCase
 {
 
     /**
-     * @before
-     */
-    protected function setUp()
-    {
-    }
-
-    /**
      * @test
      */
     public function trueAssertion()
